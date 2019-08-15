6ef910923803a36ffa71c147417337efbe1e905b
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2019-08-15 03:51:44 -0500

    (!) ports: add python3

---
+++ ports/python3/scripts/fix_bad_makefile.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+# Preface:
+# 
+# For some reason makesetup outputs these variables all
+# on one giant line, which causes them to be interpreted
+# as a command - "no such file or directory".
+#
+# Not sure if this is a due to sbase, dash, or what.
+#
+# As a fix, here we split the file on the bad line, delete it,
+# append the variables how they shold be, and put the file back together.
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+# 1) csplit file on the bad line
+#    this will output them to xx00 and xx01
+csplit Makefile  '/LOCALMODLIBS= /'
+
+# 2) append the correct line, formatted, onto xx00
+cat >> xx00 << 'EOF'
+LOCALMODLIBS=-lreadline -ltermcap -L/lib -lssl -lcrypto -lncurses -ltermcap -L$(exec_prefix)/lib -lz
+BASEMODLIBS=
+SSL=/
+PYTHONPATH=$(COREPYTHONPATH)
+COREPYTHONPATH=$(DESTPATH)$(SITEPATH)$(TESTPATH)
+TESTPATH=
+SITEPATH=
+DESTPATH=
+MACHDESTLIB=$(BINLIBDEST)
+DESTLIB=$(LIBDEST)
+EOF
+
+# 3) delete the bad line from xx01, appending remainder to xx00
+sed '1d' xx01 >> xx00
+
+# 4) rename final file
+mv -f xx00 Makefile
